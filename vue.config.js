78335dfac77949cc5e5f4a16af3617ca247784fe
Nick Thomasson <68825938+nickThomasson@users.noreply.github.com>
2020-07-28 21:18:49 +0200

    Feature/implement error message (#11)
    * Change grid on detail page
    
    * Add simple error message
    
    * Change page title in config file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "Zeltplatz-Datenbank";
+      return args;
+    });
+  },
   transpileDependencies: ["vuetify"]
 };
