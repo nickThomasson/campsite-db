590bb2045bea405f68657fc87c04c5c0ef3001fe
Dominik Niemann <dominikniemann@posteo.de>
2020-07-31 21:07:25 +0200

    Change navigation entry

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,7 +17,7 @@
         </v-list-item-icon>
         <v-list-item-content>
           <v-list-item-title>{{
-            i18n.CAMPSITE_NAVIGATION_STARTPAGE
+            i18n.CAMPSITE_SEARCH_TITLE
           }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
