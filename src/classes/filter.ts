fc51fd18d54674c3268e98b277b5fc95e6dcdbc6
Nick Thomasson <68825938+nickThomasson@users.noreply.github.com>
2020-08-07 21:23:25 +0200

    Add filter classes (#58)

---
+++ src/classes/filter.ts
@@ -0,0 +1,45 @@
+interface QueryParameter {
+  field: string;
+  operator: string;
+  value: string;
+}
+
+interface FilterValues {
+  filterName: string;
+  isActive: boolean;
+  rawValue: string | boolean | Array<string>;
+}
+
+abstract class CreateFilterQuery {
+  protected queryParameter: QueryParameter;
+  protected query: string;
+
+  constructor(queryParameter: QueryParameter) {
+    this.queryParameter = queryParameter;
+    this.query = `&filter[${this.queryParameter.field}][${this.queryParameter.operator}]=${this.queryParameter.value}`;
+  }
+}
+
+abstract class CreateFilterValues extends CreateFilterQuery {
+  protected filterValues: FilterValues;
+
+  constructor(filterValues: FilterValues, queryParameter: QueryParameter) {
+    super(queryParameter);
+    this.filterValues = filterValues;
+  }
+}
+
+export class CreateFilter extends CreateFilterValues {
+  constructor(filterValues: FilterValues, queryParameter: QueryParameter) {
+    super(filterValues, queryParameter);
+  }
+
+  get() {
+    return {
+      filterName: this.filterValues.filterName,
+      isActive: this.filterValues.isActive,
+      rawValue: this.filterValues.rawValue,
+      query: this.query
+    };
+  }
+}
