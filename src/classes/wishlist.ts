931e27682233f3a5068561cc2cffc25cbd76e326
Dominik Niemann <dominikniemann@posteo.de>
2020-08-09 14:08:47 +0200

    Fix wishlist item location

--- src/classes/wishlist.ts
+++ src/classes/wishlist.ts
@@ -1,25 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ReturnId } from "@/classes/interfaces";
+import { find } from "lodash";
 
 interface Wishlist {
-  [key: string]: { [key: string]: string };
+  [key: string]: { [key: string]: string } | any;
 }
 
 export class WishlistItem implements ReturnId {
   private item: Wishlist;
+  private address: any;
 
   constructor(item: Wishlist) {
     this.item = item;
+    this.address = find(item.address, { mainAddress: true }) || item.address[0];
   }
 
   get() {
     return {
       id: this.item["id"],
       title: this.item["name"],
-      zip: this.item["address"]["zip"],
-      city: this.item["address"]["city"],
-      county: this.item["address"]["county"],
-      street: this.item["address"]["street"],
-      houseNumber: this.item["address"]["houseNumber"],
+      zip: this.address["zip"],
+      city: this.address["city"],
+      county: this.address["county"],
+      street: this.address["street"],
+      houseNumber: this.address["houseNumber"],
       type: "rooms" in this.item ? "house" : "campsite"
     };
   }
