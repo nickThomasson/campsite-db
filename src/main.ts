4fd4eaf7bf16dd106d79cfc8f6a7dda02cd7e4c2
Nick Thomasson <68825938+nickThomasson@users.noreply.github.com>
2020-08-10 21:03:45 +0200

    Add sentry monitoring (#72)

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,16 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "./styles/main.scss";
 import { writeToStorage } from "@/helper/localStorage";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
 
 Vue.config.productionTip = false;
+
+Sentry.init({
+  dsn:
+    "https://fae3c24da6be49f998c65f1b74d52d67@o432353.ingest.sentry.io/5384965",
+  integrations: [new VueIntegration({ Vue, attachProps: true })]
+});
 
 store.subscribe((mutation: any, state: any) => {
   const wishlist = state.wishlist;
